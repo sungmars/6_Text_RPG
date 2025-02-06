cf62faf68cb6eb94fc20d11c5d551f4d8a86a0d0
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-06 15:54:41 +0900

    초기 커밋.

---
+++ 6_Text_RPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _6_Text_RPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
